05af6eba35121c45d1533f0161805424f03abdd8
reginaldodlneto <reginaldo.dlneto@gmail.com>
2021-08-18 18:18:50 -0300

    adding header

---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import Header from '../partials/Header/Header'
+
+const Home = () => {
+  return <Header />
+}
+
+export default Home
---
+++ src/partials/Header/Header.style.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+// JSS - Javascript Style Sheet
+const useStyles = makeStyles(() => ({
+  // Retorno Implicito
+  title: {
+    flexGrow: 1,
+    marginLeft: 10
+  }
+}))
+
+export default useStyles
